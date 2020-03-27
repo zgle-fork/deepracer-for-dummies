ec58377949d587d6d49c3521ea810b9f2d4cb7bf
Ubuntu <ubuntu@ip-10-212-202-124.ec2.internal>
2020-03-27 21:42:44 +0000

    u

--- scripts/1-prerequisites.sh
+++ scripts/1-prerequisites.sh
@@ -32,6 +32,7 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+echo "distribution: $distribution"
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
