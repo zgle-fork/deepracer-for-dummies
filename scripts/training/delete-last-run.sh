0a0c5ff1a442e936dcab69b4ef75caeca38ed8b3
Ubuntu <ubuntu@ip-10-212-202-122.ec2.internal>
2020-03-28 22:55:58 +0000

    mkdir -p

--- scripts/training/delete-last-run.sh
+++ scripts/training/delete-last-run.sh
@@ -2,6 +2,6 @@
 
 rm -rf ../../docker/volumes/minio/bucket/rl-deepracer-sagemaker
 rm -rf ../../docker/volumes/robo/checkpoint/checkpoint
-mkdir ../../docker/volumes/robo/checkpoint/checkpoint
+mkdir -p ../../docker/volumes/robo/checkpoint/checkpoint
 rm -rf /robo/container/*
 rm -rf ../../docker/volumes/robo/checkpoint/log/*
